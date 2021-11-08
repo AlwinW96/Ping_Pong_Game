513303f1e09f55f256862482f4a3ee66602823b4
alwinW96 <99060433@mydavinci.nl>
2021-11-08 12:25:00 +0100

    Start of Ping_Pong_Game

---
+++ Ping_Pong_Game.py
@@ -0,0 +1,25 @@
+#Alwin Wezenbeek 99060433
+import pygame
+
+
+# -------- CONSTANTEN EN GLOBALE VARIABELEN --------
+WINDOW_SIZE = [1050, 750] #venstergrootte
+
+
+
+# -------- FUNCTIE DEFINITIES --------
+
+
+
+# -------- PYGAME INITIALISATIE --------
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE) #open scherm
+pygame.display.set_caption("Scherm titel") #geef een titel aan het scherm
+# -------- HOOFDLOOP VAN HET PROGRAMMA --------
+
+
+
+
+
+# -------- AFSLUITING --------
+pygame.quit() #sluit scherm af
